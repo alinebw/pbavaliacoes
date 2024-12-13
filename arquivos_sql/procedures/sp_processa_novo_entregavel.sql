95ab0e92493c87bdc2edcbeea7a383d84f335b1a
Aline Branco <79863993+alinebw@users.noreply.github.com>
2024-12-13 13:15:33 -0300

    Update sp_processa_novo_entregavel.sql

--- arquivos_sql/procedures/sp_processa_novo_entregavel.sql
+++ arquivos_sql/procedures/sp_processa_novo_entregavel.sql
@@ -1,49 +1,44 @@
 DELIMITER //
 
 CREATE PROCEDURE sp_processa_novo_entregavel (
-    IN p_id_entregavel VARCHAR(255)
+    IN p_id_entregavel VARCHAR(45)
 )
 BEGIN
     DECLARE invalid_data BOOL DEFAULT FALSE;
 
-    -- Verifica condições relacionadas ao ID e status do entregável
-    IF NOT EXISTS (SELECT 1 FROM entregaveis WHERE id_entregavel = p_id_entregavel) THEN
-        SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'Entregável não encontrado.';
-    ELSEIF EXISTS (SELECT 1 FROM entregaveis WHERE id_entregavel = p_id_entregavel AND status = 'PROCESSADO') THEN
-        -- Não executa o processamento
-        LEAVE sp_processa_novo_entregavel;
-    END IF;
+    sp_processa_novo_entregavel_block: BEGIN
+        -- Verifica condições relacionadas ao ID e status do entregável
+        IF NOT EXISTS (SELECT 1 FROM entregaveis WHERE id_entregavel = p_id_entregavel) THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'Entregável não encontrado.';
+        ELSEIF EXISTS (SELECT 1 FROM entregaveis WHERE id_entregavel = p_id_entregavel AND status = 'PROCESSADO') THEN
+            -- Não executa o processamento
+            LEAVE sp_processa_novo_entregavel_block;
+        END IF;
 
-    -- Start Transaction
-    START TRANSACTION;
+        -- Verifica integridade dos dados
+        SELECT 1 INTO invalid_data
+        FROM entregaveis
+        WHERE id_entregavel = p_id_entregavel
+          AND (id_avaliacao IS NULL OR data_recebimento IS NULL);
 
-    -- Verifica integridade dos dados
-    SELECT 1 INTO invalid_data
-    FROM entregaveis
-    WHERE id_entregavel = p_id_entregavel
-      AND (id_avaliacao IS NULL OR data_recebimento IS NULL);
+        IF invalid_data THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'Dados inválidos ou incompletos no entregável.';
+        END IF;
 
-    IF invalid_data THEN
-        ROLLBACK;
-        SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'Dados inválidos ou incompletos no entregável.';
-    END IF;
+        -- Atualiza status
+        UPDATE entregaveis
+        SET
+            data_processamento = NOW(),
+            status = 'PROCESSADO'
+        WHERE id_entregavel = p_id_entregavel;
 
-    -- Preenche campo status
-    UPDATE entregaveis
-    SET
-        data_processamento = NOW(),
-        status = 'PROCESSADO'
-    WHERE id_entregavel = p_id_entregavel;
+        -- Registra log de processamento
+        INSERT INTO logs_processamento (id_entregavel, data_processamento, status, mensagem)
+        VALUES (p_id_entregavel, NOW(), 'PROCESSADO', 'Dados processados com sucesso.');
+    END sp_processa_novo_entregavel_block;
 
-    -- Insere um novo log de processamento com o status 'PROCESSADO'
-    INSERT INTO log_processamento (id_entregavel, data_processamento, status, mensagem)
-    VALUES (p_id_entregavel, NOW(), 'PROCESSADO', 'Dados processados com sucesso.');
-
-    -- Commit Transaction
-    COMMIT;
-
-END//
+END //
 
 DELIMITER ;
