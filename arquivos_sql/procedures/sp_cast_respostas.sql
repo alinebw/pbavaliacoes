07343a5167d52d0c8f530f06b7e8451d23a528d6
Aline Branco <79863993+alinebw@users.noreply.github.com>
2024-12-13 13:18:01 -0300

    Update and rename sp_cast_csat_nps.sql to sp_cast_respostas.sql

---
+++ arquivos_sql/procedures/sp_cast_respostas.sql
@@ -0,0 +1,69 @@
+-- Procedure cast respostas da view perguntas_respostas
+
+DELIMITER //
+
+CREATE PROCEDURE sp_cast_respostas(
+    IN p_id_entregavel VARCHAR(45)
+)
+BEGIN
+    DECLARE v_csat_consultor DECIMAL(5, 2) DEFAULT NULL;
+    DECLARE v_csat_evento DECIMAL(5, 2) DEFAULT NULL;
+    DECLARE v_nps DECIMAL(5, 2) DEFAULT NULL;
+    DECLARE v_nps_status ENUM('PROMOTOR', 'NEUTRO', 'DETRATOR');
+    DECLARE v_csat_conteudo_opcao VARCHAR(20) DEFAULT NULL; -- Para armazenar o texto completo
+    DECLARE v_csat_conteudo_opcao_online VARCHAR(20) DEFAULT NULL;
+    DECLARE v_plataforma_acessivel VARCHAR(3) DEFAULT NULL;
+    DECLARE v_comentario_obrigatorio TEXT DEFAULT ''; -- Valor padrão vazio
+    DECLARE v_comentario_opcional TEXT DEFAULT NULL;
+    DECLARE v_nome_respondente VARCHAR(100) DEFAULT NULL;
+
+    -- Pega os valores da view
+    SELECT 
+        MAX(CASE WHEN ref = 'csat_consultor' THEN CAST(valor_resposta AS DECIMAL(5, 2)) END),
+        MAX(CASE WHEN ref = 'csat_evento' THEN CAST(valor_resposta AS DECIMAL(5, 2)) END),
+        MAX(CASE WHEN ref = 'nps' THEN CAST(valor_resposta AS DECIMAL(5, 2)) END),
+        MAX(CASE WHEN ref = 'csat_conteudo' THEN texto_resposta END),
+        MAX(CASE WHEN ref = 'extra' THEN texto_resposta END),
+        MAX(CASE WHEN ref = 'comentario_obrigatorio' THEN texto_resposta END),
+        MAX(CASE WHEN ref = 'comentario_opcional' THEN texto_resposta END),
+        MAX(CASE WHEN ref = 'nome' THEN texto_resposta END)
+    INTO 
+        v_csat_consultor, 
+        v_csat_evento, 
+        v_nps, 
+        v_csat_conteudo_opcao, 
+        v_plataforma_acessivel, 
+        v_comentario_obrigatorio, 
+        v_comentario_opcional, 
+        v_nome_respondente
+    FROM vw_perguntas_respostas
+    WHERE id_entregavel = p_id_entregavel;
+
+    -- Determina o status do NPS
+    IF v_nps >= 9 THEN
+        SET v_nps_status = 'PROMOTOR';
+    ELSEIF v_nps BETWEEN 7 AND 8 THEN
+        SET v_nps_status = 'NEUTRO';
+    ELSEIF v_nps <= 6 THEN
+        SET v_nps_status = 'DETRATOR';
+    ELSE
+        SET v_nps_status = NULL; -- Para casos de erro
+    END IF;
+
+    -- Atualiza os valores na tabela entregaveis
+    UPDATE entregaveis
+    SET
+        csat_consultor = v_csat_consultor,
+        csat_evento = v_csat_evento,
+        nps = v_nps,
+        nps_status = v_nps_status,
+        csat_conteudo_opcao = v_csat_conteudo_opcao,
+        plataforma_acessivel = v_plataforma_acessivel,
+        comentario_obrigatorio = v_comentario_obrigatorio,
+        comentario_opcional = v_comentario_opcional,
+        nome_respondente = v_nome_respondente
+    WHERE id_entregavel = p_id_entregavel;
+
+END //
+
+DELIMITER ;
